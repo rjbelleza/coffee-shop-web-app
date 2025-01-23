41f3a9c228a89fd03af48830615e67617e9b2938
rjbelleza <rjbelleza19@gmail.com>
2025-01-23 21:55:41 +0800

    Created Login and Signup nav links

--- index.php
+++ index.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Cozy Bean | Coffee Shop </title>
+    <title> The Cozy Bean | Coffee Shop </title>
 </head>
 <body>
-
+    <style>
+        .forms {
+            height: 100px;
+            width: 100px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 20px;
+            background-color: gray;
+        }
+    </style>
+    <main>
+        <div class="forms">
+            <a href="pages/login.php" id="login"> LOGIN </a>
+            <a href="pages/signup.php" id="signup"> SIGN UP </a>
+        </div>
+    </main>
 </body>
 </html>
