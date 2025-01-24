c23c45624a24158e983a4f9cd8e58f6c928ac9cc
rjbelleza <rjbelleza19@gmail.com>
2025-01-24 13:15:01 +0800

    Created users table

---
+++ includes/databaseTables.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+$table = "users";
+
+$table = preg_replace('/[^a-zA-Z0-9_]/', '', $table);
+
+$createTable = "CREATE TABLE $table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fullname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL
+);";
+
+try {
+
+    require_once "databaseConnection.inc.php";
+
+    $stmt = $pdo->prepare($createTable);
+    $stmt->execute();
+
+    $pdo = null;
+    $stmt = null;
+
+    die();
+
+} catch (PDOException $e) {
+    die("Query Failed: " . $e->getMessage());
+}
